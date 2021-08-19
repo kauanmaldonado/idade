e6818811b01ffcbac4dbb5c31b18cb36d4f9c14b
kauanmaldonado <kauan.maldonado16@gmail.com>
2021-08-19 15:47:24 -0300

    Initial commit

---
+++ src/main/java/IDADE/maioroumenor.java
@@ -0,0 +1,24 @@
+package IDADE;
+
+import java.util.Scanner;
+
+public class maioroumenor {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Qual o seu nome?");
+        String nome = input.nextLine();
+
+        System.out.println("Qual a sua idade?");
+        int idade = input.nextInt();
+
+        if(idade < 18){
+            System.out.println("Você é menor de idade");
+        }
+
+    }
+}
+
+
